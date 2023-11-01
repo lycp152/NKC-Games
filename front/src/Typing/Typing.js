a9c23fb217b5fcdf1c8c4157eb5ba82eb255e87f
zinaung <nkc20222367@st.denpa.jp>
2023-11-01 14:53:30 +0900

    問題文を作ってランダムで表示してみよう

--- front/src/Typing/Typing.js
+++ front/src/Typing/Typing.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Typing.css";
 
-//JavaScriptのコードはここに記入
-
 const Typing = () => {
+  useEffect(() => {
+    let Q = [
+      "apple",
+      "banana",
+      "melon",
+      "mango",
+      "strawberry",
+      "blueberry",
+      "orange",
+    ]; //問題文
+    let Q_No = Math.floor(Math.random() * Q.length); //問題をランダムで出題する
+
+    function push_Keydown(event) {
+      let keyCode = event.key;
+      document.getElementById("push").innerHTML = keyCode + "を押しました";
+      document.getElementById("start").innerHTML = Q[Q_No];
+    }
+
+    window.addEventListener("keydown", push_Keydown);
+  }, []);
+
   return (
     <>
-      <h2>タイピングゲーム</h2>
-      <p>ここはタイピングゲームの画面です。</p>
+      <h1 id="push">何かキーを押して下さい</h1>
+      <h1 id="start">問題をここに出題します</h1>
     </>
   );
 };
