75213b41e976a55a7193e978d369089650608201
aonana <nkc20222428@st.denpa.jp>
2023-11-02 10:38:28 +0900

    ゲームオーバー

--- front/src/Breakout/Breakout.js
+++ front/src/Breakout/Breakout.js
@@ -64,8 +64,16 @@ const Breakout = () => {
       if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
       }
-      if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+      if (y + dy < ballRadius) {
         dy = -dy;
+      } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+          dy = -dy;
+        } else {
+          alert("GAME OVER");
+          document.location.reload();
+          clearInterval(interval);
+        }
       }
 
       if (rightPressed) {
@@ -84,7 +92,7 @@ const Breakout = () => {
       y += dy;
     }
 
-    setInterval(draw, 10);
+    const interval = setInterval(draw, 10);
   }, []);
 
   return (
