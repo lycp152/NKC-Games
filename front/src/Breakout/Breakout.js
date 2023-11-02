1fd598e8fd081e210a41e0460ff0738a93735a37
aonana <nkc20222428@st.denpa.jp>
2023-11-02 10:27:52 +0900

    パドルとキーボード操作

--- front/src/Breakout/Breakout.js
+++ front/src/Breakout/Breakout.js
@@ -4,13 +4,37 @@ import "./Breakout.css";
 const Breakout = () => {
   useEffect(() => {
     //JavaScriptのコードはここに記入
-    const canvas = document.getElementById("myCanvas");
-    const ctx = canvas.getContext("2d");
-    let x = canvas.width / 2;
-    let y = canvas.height - 30;
-    let dx = 2;
-    let dy = -2;
-    const ballRadius = 10;
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var ballRadius = 10;
+    var x = canvas.width / 2;
+    var y = canvas.height - 30;
+    var dx = 2;
+    var dy = -2;
+    var paddleHeight = 10;
+    var paddleWidth = 75;
+    var paddleX = (canvas.width - paddleWidth) / 2;
+    var rightPressed = false;
+    var leftPressed = false;
+
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+
+    function keyDownHandler(e) {
+      if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+      } else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+      }
+    }
+
+    function keyUpHandler(e) {
+      if (e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+      } else if (e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+      }
+    }
 
     function drawBall() {
       ctx.beginPath();
@@ -19,18 +43,45 @@ const Breakout = () => {
       ctx.fill();
       ctx.closePath();
     }
+    function drawPaddle() {
+      ctx.beginPath();
+      ctx.rect(
+        paddleX,
+        canvas.height - paddleHeight,
+        paddleWidth,
+        paddleHeight
+      );
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+    }
 
     function draw() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       drawBall();
-      x += dx;
-      y += dy;
+      drawPaddle();
+
       if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
       }
       if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
         dy = -dy;
       }
+
+      if (rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width) {
+          paddleX = canvas.width - paddleWidth;
+        }
+      } else if (leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0) {
+          paddleX = 0;
+        }
+      }
+
+      x += dx;
+      y += dy;
     }
 
     setInterval(draw, 10);
