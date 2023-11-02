6ab2bff868580ce0ee0cb7ff1226387ee1cc1a25
zinaung <nkc20222367@st.denpa.jp>
2023-11-02 11:44:01 +0900

    効果音をつけてみよう

--- front/src/Typing/Typing.js
+++ front/src/Typing/Typing.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import "./Typing.css";
+import okAudio from "./ok.mp3";
+import goodAudio from "./good.mp3";
 
 const Typing = () => {
   useEffect(() => {
@@ -21,14 +23,14 @@ const Typing = () => {
 
     function push_Keydown(event) {
       let keyCode = event.key;
-      if (Q_l == Q_l - Q_i) {
+      if (Q_l === Q_l - Q_i) {
         document.getElementById("start").innerHTML = Q[Q_No].substring(
           Q_i,
           Q_l
         ); //問題を書き出す
       }
 
-      if (Q[Q_No].charAt(Q_i) == keyCode) {
+      if (Q[Q_No].charAt(Q_i) === keyCode) {
         //押したキーが合っていたら
 
         Q_i++; //判定する文章に１足す
@@ -40,7 +42,8 @@ const Typing = () => {
         if (Q_l - Q_i === 0) {
           //全部正解したら
 
-          new Audio("ok.mp3").play(); // 音声再生;
+          console.log("okAudio", okAudio);
+          new Audio(okAudio).play(); // "ok.mp3" オーディオを再生
 
           Q_No = Math.floor(Math.random() * Q.length); //問題をランダムで出題する
           Q_i = 0; //回答初期値・現在どこまで合っているか判定している文字番号
@@ -51,7 +54,8 @@ const Typing = () => {
             Q_l
           ); //新たな問題を書き出す
         } else {
-          new Audio("good.mp3").play(); // 音声再生;
+          console.log("goodAudio", goodAudio);
+          new Audio(goodAudio).play(); // "good.mp3" オーディオを再生
         }
       }
     }
