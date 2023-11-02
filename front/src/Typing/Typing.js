f525d78d8d29cfda0a15c5c81656d01d2b619145
zinaung <nkc20222367@st.denpa.jp>
2023-11-02 11:21:45 +0900

    効果音をつけてみよう

--- front/src/Typing/Typing.js
+++ front/src/Typing/Typing.js
@@ -40,6 +40,8 @@ const Typing = () => {
         if (Q_l - Q_i === 0) {
           //全部正解したら
 
+          new Audio("ok.mp3").play(); // 音声再生;
+
           Q_No = Math.floor(Math.random() * Q.length); //問題をランダムで出題する
           Q_i = 0; //回答初期値・現在どこまで合っているか判定している文字番号
           Q_l = Q[Q_No].length; //計算用の文字の長さ
@@ -48,6 +50,8 @@ const Typing = () => {
             Q_i,
             Q_l
           ); //新たな問題を書き出す
+        } else {
+          new Audio("good.mp3").play(); // 音声再生;
         }
       }
     }
@@ -56,7 +60,7 @@ const Typing = () => {
   return (
     <>
       <center>
-        <h1 id="start" class="text">
+        <h1 id="start" className="text">
           何かキーを押して下さい
         </h1>
       </center>
