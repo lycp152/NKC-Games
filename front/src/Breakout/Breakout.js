12fff7d20c8c6fd92c1fb4be06adceb07406679e
aonana <nkc20222428@st.denpa.jp>
2023-11-08 14:43:54 +0900

    マウス操作

--- front/src/Breakout/Breakout.js
+++ front/src/Breakout/Breakout.js
@@ -35,6 +35,7 @@ const Breakout = () => {
 
     document.addEventListener("keydown", keyDownHandler, false);
     document.addEventListener("keyup", keyUpHandler, false);
+    document.addEventListener("mousemove", mouseMoveHandler, false);
 
     function keyDownHandler(e) {
       if (e.key == "Right" || e.key == "ArrowRight") {
@@ -49,6 +50,13 @@ const Breakout = () => {
         rightPressed = false;
       } else if (e.key == "Left" || e.key == "ArrowLeft") {
         leftPressed = false;
+      }
+    }
+
+    function mouseMoveHandler(e) {
+      var relativeX = e.clientX - canvas.offsetLeft;
+      if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
       }
     }
     function collisionDetection() {
