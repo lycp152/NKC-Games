a68d337ba6207984f8f80129546fbbdf86bcdc48
aonana <nkc20222428@st.denpa.jp>
2023-12-01 11:10:53 +0900

    スタートボタン実装

--- front/src/Breakout/Breakout.js
+++ front/src/Breakout/Breakout.js
@@ -1,35 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Breakout.css";
 
 const Breakout = () => {
-  useEffect(() => {
-    //JavaScriptのコードはここに記入
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    var ballRadius = 10;
-    var x = canvas.width / 2;
-    var y = canvas.height - 30;
-    var dx = 2;
-    var dy = -2;
-    var paddleHeight = 30;
-    var paddleWidth = 90;
-    var paddleX = (canvas.width - paddleWidth) / 2;
-    var rightPressed = false;
-    var leftPressed = false;
-    var brickRowCount = 6;
-    var brickColumnCount = 6;
-    var brickWidth = 80;
-    var brickHeight = 25;
-    var brickPadding = 10;
-    var brickOffsetTop = 30;
-    var brickOffsetLeft = 30;
-    var score = 0;
-    var lives = 3;
+  const [gameState, setGameState] = useState("start");
 
-    var bricks = [];
-    for (var c = 0; c < brickColumnCount; c++) {
+  function startGame() {
+    setGameState("playing");
+    // ゲームが開始されたときの追加のセットアップやアクション
+  }
+
+  function gameButton() {
+    if (gameState === "start") {
+      startGame();
+    }
+  }
+
+  useEffect(() => {
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+    let ballRadius = 10;
+    let x = canvas.width / 2;
+    let y = canvas.height - 30;
+    let dx = 2;
+    let dy = -2;
+    let paddleHeight = 30;
+    let paddleWidth = 90;
+    let paddleX = (canvas.width - paddleWidth) / 2;
+    let rightPressed = false;
+    let leftPressed = false;
+    let brickRowCount = 6;
+    let brickColumnCount = 6;
+    let brickWidth = 80;
+    let brickHeight = 25;
+    let brickPadding = 10;
+    let brickOffsetTop = 30;
+    let brickOffsetLeft = 30;
+    let score = 0;
+    let lives = 3;
+
+    let bricks = [];
+    for (let c = 0; c < brickColumnCount; c++) {
       bricks[c] = [];
-      for (var r = 0; r < brickRowCount; r++) {
+      for (let r = 0; r < brickRowCount; r++) {
         bricks[c][r] = { x: 0, y: 0, status: 1 };
       }
     }
@@ -55,15 +67,16 @@ const Breakout = () => {
     }
 
     function mouseMoveHandler(e) {
-      var relativeX = e.clientX - canvas.offsetLeft;
+      let relativeX = e.clientX - canvas.offsetLeft;
       if (relativeX > 0 && relativeX < canvas.width) {
         paddleX = relativeX - paddleWidth / 2;
       }
     }
+
     function collisionDetection() {
-      for (var c = 0; c < brickColumnCount; c++) {
-        for (var r = 0; r < brickRowCount; r++) {
-          var b = bricks[c][r];
+      for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+          let b = bricks[c][r];
           if (b.status === 1) {
             if (
               x > b.x &&
@@ -91,6 +104,7 @@ const Breakout = () => {
       ctx.fill();
       ctx.closePath();
     }
+
     function drawPaddle() {
       ctx.beginPath();
       ctx.rect(
@@ -103,31 +117,27 @@ const Breakout = () => {
       ctx.fill();
       ctx.closePath();
     }
+
     function drawBricks() {
-      for (var c = 0; c < brickColumnCount; c++) {
-        for (var r = 0; r < brickRowCount; r++) {
+      for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
           if (bricks[c][r].status === 1) {
-            var brickX = r * (brickWidth + brickPadding) + brickOffsetLeft;
-            var brickY = c * (brickHeight + brickPadding) + brickOffsetTop;
+            let brickX = r * (brickWidth + brickPadding) + brickOffsetLeft;
+            let brickY = c * (brickHeight + brickPadding) + brickOffsetTop;
             bricks[c][r].x = brickX;
             bricks[c][r].y = brickY;
             ctx.beginPath();
             ctx.rect(brickX, brickY, brickWidth, brickHeight);
             if (c === 0) {
               ctx.fillStyle = "#0000FF";
-              // 2行目
             } else if (c === 1) {
               ctx.fillStyle = "#0066FF";
-              // 3行目
             } else if (c === 2) {
               ctx.fillStyle = "#5D99FF";
-              // 4行目
             } else if (c === 3) {
               ctx.fillStyle = "#A4C6FF";
-              // 5行目
             } else if (c === 4) {
               ctx.fillStyle = "b3d9ff";
-              //6行目
             } else if (c === 5) {
               ctx.fillStyle = "b3f2ff";
             }
@@ -137,15 +147,16 @@ const Breakout = () => {
         }
       }
     }
+
     function drawScore() {
       ctx.font = "16px Arial";
       ctx.fillStyle = "#0095DD";
-      ctx.fillText("Score: " + score, 8, 20);
+      ctx.fillText("スコア: " + score, 8, 20);
     }
+
     function drawLives() {
-      ctx.font = "16px Arial";
-      ctx.fillStyle = "#0095DD";
-      // ★ 自分で修正(ライフの表示を❤︎表示に修正) ★
+      ctx.font = "20px Arial";
+      ctx.fillStyle = "#ff69b4";
       if (lives === 3) {
         ctx.fillText("❤︎❤︎❤︎", canvas.width - 85, 20);
       } else if (lives === 2) {
@@ -154,64 +165,72 @@ const Breakout = () => {
         ctx.fillText("❤︎", canvas.width - 85, 20);
       }
     }
+
     function draw() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      drawBricks();
-      drawBall();
-      drawPaddle();
-      drawScore();
-      drawLives();
-      collisionDetection();
 
-      if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-      }
-      if (y + dy < ballRadius) {
-        dy = -dy;
-      } else if (y + dy > canvas.height - ballRadius) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
+      if (gameState === "playing") {
+        drawBricks();
+        drawBall();
+        drawPaddle();
+        drawScore();
+        drawLives();
+        collisionDetection();
+
+        if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+          dx = -dx;
+        }
+        if (y + dy < ballRadius) {
           dy = -dy;
-        } else {
-          lives--;
-          if (!lives) {
-            alert("GAME OVER");
-            document.location.reload();
+        } else if (y + dy > canvas.height - ballRadius) {
+          if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
           } else {
-            x = canvas.width / 2;
-            y = canvas.height - 30;
-            dx = 3;
-            dy = -3;
-            paddleX = (canvas.width - paddleWidth) / 2;
+            lives--;
+            if (!lives) {
+              alert("ゲームオーバー");
+              document.location.reload();
+            } else {
+              x = canvas.width / 2;
+              y = canvas.height - 30;
+              dx = 3;
+              dy = -3;
+              paddleX = (canvas.width - paddleWidth) / 2;
+            }
           }
         }
-      }
 
-      if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += 7;
-      } else if (leftPressed && paddleX > 0) {
-        paddleX -= 7;
-      }
+        if (rightPressed && paddleX < canvas.width - paddleWidth) {
+          paddleX += 7;
+        } else if (leftPressed && paddleX > 0) {
+          paddleX -= 7;
+        }
 
-      x += dx;
-      y += dy;
-      requestAnimationFrame(draw);
+        x += dx;
+        y += dy;
+        requestAnimationFrame(draw);
+      }
     }
 
     draw();
-  }, []);
+  }, [gameState]);
+
   return (
     <>
       <h2>ブロック崩し</h2>
-      <p>ここはブロック崩しの画面です。</p>
       <canvas id="myCanvas" width="600" height="500"></canvas>
 
-      <button
-        id="start"
-        onclick="gameButton();startTimer()"
-        class="btn-circle-border-double"
-      >
-        START
-      </button>
+      {gameState === "start" && (
+        <div className="btn-container">
+          <button
+            id="start"
+            onClick={gameButton}
+            className="btn-circle-border-double"
+          >
+            START
+          </button>
+        </div>
+      )}
     </>
   );
 };
